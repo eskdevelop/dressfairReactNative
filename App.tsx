1aedafe6ccfed33523eb944bcceaad9a9de71589
Amjad khan <amjadbusinessarcade@gmail.com>
2026-04-30 11:30:27 +0500

    added some data in git ignore

--- App.tsx
+++ App.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
-
 import { AppRoot } from './src/app/AppRoot';
 import { store } from './src/app/store';
 
-export default function App() {
+export default function App(){
   return (
     <Provider store={store}>
       <SafeAreaProvider>
-        <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
+        <StatusBar barStyle="dark-content" backgroundColor="#ffffff"/>
         <AppRoot />
       </SafeAreaProvider>
     </Provider>
